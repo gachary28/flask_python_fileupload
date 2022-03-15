90db32c106ed3ac3e8d2cc5a0d5ba689c0e7adbf
Ganesh Achary <32837018+gachary28@users.noreply.github.com>
2022-03-15 21:12:46 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import os
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+print(__name__)
+UPLOAD_FOLDER = 'D://Applicationtesting//file_upload_python//'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/')
+def upload_file():   
+ return render_template('upload.html')
+
+@app.route('/display', methods = ['GET', 'POST'])
+def save_file():
+    if request.method == 'POST':
+        try:
+            f = request.files['file']
+            filename = secure_filename(f.filename)
+            f.save(app.config['UPLOAD_FOLDER'] + filename)
+            ##file = open(app.config['UPLOAD_FOLDER'] + filename,"r")
+            ##content = file.read()
+            return render_template('display.html', content="File Uploaded Successfully") 
+        except:
+            return render_template('display.html', content="Failed to upload the file") 
+
+
+if __name__==('__main__'):
+    app.run(debug=True)
